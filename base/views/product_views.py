1b12fa7316c41118ba2fd59269c2d9903b7b0ac9
rajat-gith <rajatnayak1582002@gmail.com>
2022-08-12 21:52:30 +0530

    Initial_commit

---
+++ base/views/product_views.py
@@ -0,0 +1,71 @@
+import imp
+from itertools import product
+from operator import imod
+from django.shortcuts import render
+from django.http import JsonResponse
+from base.products import products
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Product,Review 
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+
+from base.serializer import ProductSerializer,UserSerializer,UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+@api_view(['GET'])
+def getProducts(request):
+    products=Product.objects.all()
+    serializer=ProductSerializer(products,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product=Product.objects.get(_id=pk)
+    serializer=ProductSerializer(product,many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id=pk)
+    data = request.data
+
+    # 1 - Review already exists
+    alreadyExists = product.review_set.filter(user=user).exists()
+    if alreadyExists:
+        content = {'detail': 'Product already reviewed'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # 2 - No Rating or 0
+    elif data['rating'] == 0:
+        content = {'detail': 'Please select a rating'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # 3 - Create review
+    else:
+        review = Review.objects.create(
+            user=user,
+            product=product,
+            name=user.first_name,
+            rating=data['rating'],
+            comment=data['comment'],
+        )
+
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+
+        total = 0
+        for i in reviews:
+            total += i.rating
+
+        product.rating = total / len(reviews)
+        product.save()
+
+        return Response('Review Added')
